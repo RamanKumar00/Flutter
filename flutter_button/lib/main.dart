b866b811681f8161d42af8075fdb9e944393ef0f
RamanKumar00 <ramankr7321@gmail.com>
2025-01-04 23:11:04 +0530

    first commit

---
+++ flutter_button/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String? get flutter => null;
+
+  get button => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text("Flutter buttons"),
+        ),
+        body: TextButton(
+          child: Text('Click here!!'),
+          onPressed: () {
+            print('Button Clicked ');
+          },
+          onLongPress: () {
+            print('onlong pressed');
+          },
+        ));
+  }
+}
